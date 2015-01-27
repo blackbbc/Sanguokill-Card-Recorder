6a9c735be839d00cb05e7a19630def681a29b055
blackbbc <505968815@qq.com>
2015-01-27 13:38:57 +0800

    update UI and JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
-var phonecatApp = angular.module('phonecatApp', ['ngMaterial']);
+var App = angular.module('sgsRecorder', ['ngMaterial', 'ngAnimate']);
 
-phonecatApp.controller('PhoneListCtrl',['$scope', '$http',
-    function($scope, $http) {
+App.controller('main',['$scope',
+  function($scope) {
 
-        $scope.name = "sweet";
+    $scope.def=[30, 24, 12, 9, 5, 5, 3, 6, 5, 1, 3, 1, 4, 2, 2, 3, 2, 7, 3, 2, 6];
+    $scope.card = [];
 
-    }]);
+    $scope.refresh = function(index) {
+      if (index == -1) {
+        for (i=0; i<21; i++) {
+          $scope.card[i] = $scope.def[i];
+        }
+        return;
+      }
+
+      if ($scope.card[index] > 0) {
+        $scope.card[index]--;
+      }
+    }
+
+    $scope.refresh(-1);
+
+  }]);
